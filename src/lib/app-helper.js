8cff6cfed20f317ff16010d3e85ed78266273d2e
Jesse MacFadyen <purplecabbage@gmail.com>
2024-02-27 15:59:22 -0800

    pretty print config.json, utf-8 is default

--- src/lib/app-helper.js
+++ src/lib/app-helper.js
@@ -81,8 +81,9 @@ async function runScript (command, dir, cmdArgs = []) {
  * @param {object} config object to write
  */
 function writeConfig (file, config) {
+  console.log('writing config to', file)
   fs.ensureDirSync(path.dirname(file))
-  fs.writeJSONSync(file, config, { encoding: 'utf-8' })
+  fs.writeJSONSync(file, config, { spaces: 2 })
 }
 
 module.exports = {
